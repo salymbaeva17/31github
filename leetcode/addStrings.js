e7ca92e6c347b6c81a52a09dec47d75b3067f7d0
salymbaeva17 <salymbaeva17@gmail.com>
2023-12-05 23:39:26 +0600

    Add Strings

---
+++ leetcode/addStrings.js
@@ -0,0 +1,6 @@
+const addStrings = function(num1, num2) {
+    return (+num1 + +num2).toString()
+};
+console.log(addStrings('11', '123'))
+console.log(addStrings("456", "77"))
+console.log(addStrings("0", "0"))
