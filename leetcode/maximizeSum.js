2720fbcefdeebeed294a37254cfbb3fa354a3da7
salymbaeva17 <salymbaeva17@gmail.com>
2023-12-21 21:54:55 +0600

    Maximum Sum With Exactly K Elements

---
+++ leetcode/maximizeSum.js
@@ -0,0 +1,10 @@
+maximizeSum = function (nums, k) {
+    let max = Math.max(...nums);
+    let sum = 0;
+    for (let i = 0; i < k; i++) {
+        sum += max;
+        max += 1;
+    }
+    return sum;
+};
+console.log(maximizeSum([1,2,3,4,5], 3))
