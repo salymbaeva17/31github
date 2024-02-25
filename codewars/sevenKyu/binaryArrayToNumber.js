efcbcac7d53b94101cc99f82a41d1090ca03225d
salymbaeva17 <salymbaeva17@gmail.com>
2024-02-25 23:58:57 +0600

    Ones and Zeros

--- codewars/sevenKyu/binaryArrayToNumber.js
+++ codewars/sevenKyu/binaryArrayToNumber.js
@@ -1,6 +1,7 @@
 const binaryArrayToNumber = arr => {
     return arr.reverse().reduce((acc, item, idx) => acc + item*2**idx, 0)
-};
+}
+
 console.log(binaryArrayToNumber([0,0,1,0]))
 console.log(binaryArrayToNumber([0,0,0,1]))
 console.log(binaryArrayToNumber([1,1,1,1]))
