a0c6eb083d95fca6e45ded93593972a706889922
salymbaeva17 <salymbaeva17@gmail.com>
2024-03-21 23:37:18 +0600

    Promise State

--- codewars/promise/getState.js
+++ codewars/promise/getState.js
@@ -1,9 +1,16 @@
-function getState(promise) {
+async function getState(promise) {
     const t = {};
     return Promise.race([promise, t])
         .then(v => (v === t)? "pending" : "fulfilled")
         .catch(err => "rejected");
 }
-const p = Promise.reject();
+async function withNoResult(promise) {
+    const noResult = new Promise(r => setTimeout(r, 50, "NO_RESULT"));
+    return Promise.race([promise, noResult])
+}
+const p = Promise.resolve();
 const state = await withNoResult(getState(p));
 console.log(state)
+const p2 = Promise.reject();
+const state2 = await withNoResult(getState(p2));
+console.log(state2)
