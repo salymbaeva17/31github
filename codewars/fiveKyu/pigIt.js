0c1d1fd186439cb9a48aa7175b9dfaba67b8ee11
salymbaeva17 <salymbaeva17@gmail.com>
2024-04-03 23:59:46 +0600

    Simple Pig Latin

---
+++ codewars/fiveKyu/pigIt.js
@@ -0,0 +1,7 @@
+function pigIt(str) {
+    var arrayWord = str.split(' ');
+    return arrayWord.map(function(word) {
+        var firstLetter = word.charAt(0);
+        return word.slice(1) + firstLetter + 'ay';
+    }).join(' ');
+}
